0c7f7169875d4ce07a2600cba015ad8486c9fefe
Cinquin Andy <andy.cinquin@infotel.com>
2023-11-25 20:37:34 +0100

    🔧 Fix baseUrl in jsconfig.json and update import in globals.css
    The `baseUrl` in `jsconfig.json` was changed from ".." to "." to fix the path resolution. Additionally, the import statement in `globals.css` was updated to use the new package "@flaticon/flaticon-uicons/css/all/all".

--- next.config.js
+++ next.config.js
@@ -10,24 +10,6 @@ const nextConfig = {
 			},
 		],
 	},
-	i18n: {
-		localeDetection: false,
-		locales: ['fr', 'en'],
-		defaultLocale: 'fr',
-		// fixme replace that with your own url ( here is an example )
-		domains: [
-			{
-				domain: 'localhost:8080',
-				defaultLocale: 'fr',
-				locales: ['fr'],
-			},
-			{
-				domain: 'localhost:8080',
-				defaultLocale: 'en',
-				locales: ['en'],
-			},
-		],
-	},
 }
 
 module.exports = nextConfig
