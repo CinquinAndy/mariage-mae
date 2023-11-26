5c7890882d03490e40609953935b8b20bf576a3c
Cinquin Andy <andy.cinquin@infotel.com>
2023-11-26 17:05:04 +0100

    ✨ Added pages for different sections of the wedding website
    - Created new files for "adresse", "contact", "lune-de-miel", and "save-the-date" pages
    - Each page includes a layout with images, text, and custom SVG components
    - Implemented responsive design using CSS classes for different screen sizes

---
+++ src/app/contact/page.js
@@ -0,0 +1,103 @@
+import Image from 'next/image'
+import { CustomSvgComponent } from '@/components/CustomSvg.component'
+
+export default function Home() {
+	return (
+		<>
+			<div
+				className={
+					'relative -z-10 flex h-screen w-screen flex-col items-center justify-center'
+				}
+			>
+				<Image
+					src={'/willumarryme.jpg'}
+					fill={true}
+					className={'-z-10 hidden object-cover xl:block'}
+					alt={'will u marry me'}
+					quality={90}
+				/>
+				<Image
+					src={'/willumarryme_tablet.jpg'}
+					fill={true}
+					className={'-z-10 hidden object-cover md:block xl:hidden'}
+					alt={'will u marry me'}
+					quality={90}
+				/>
+				<Image
+					src={'/willumarryme_phone.jpg'}
+					fill={true}
+					className={'-z-10 block object-cover md:hidden'}
+					alt={'will u marry me'}
+					quality={90}
+				/>
+				<div
+					className={
+						'relative h-[85%] w-[90%] rounded border border-gray-100 shadow-2xl md:h-[70%] md:w-[70%] xl:h-[65%]'
+					}
+				>
+					<div className={'absolute h-full w-full p-3'}>
+						<div
+							className={
+								'relative flex h-full w-full items-center justify-center rounded bg-black/10'
+							}
+						>
+							<h1
+								className={
+									'text-center text-5xl text-white shadow-black text-shadow-lg lg:text-7xl xl:text-9xl'
+								}
+							>
+								Maeva & Romain
+							</h1>
+							<div
+								className={
+									'absolute bottom-32 left-1/2 -translate-x-1/2 transform px-4 py-2 md:bottom-8'
+								}
+							>
+								<div className={'relative h-full w-full'}>
+									<h2
+										className={
+											'text-center text-4xl text-white shadow-black text-shadow-lg'
+										}
+									>
+										10 août 2024
+									</h2>
+									<div
+										className={
+											'absolute -right-[25px] -top-[15px] flex items-center justify-center'
+										}
+									>
+										<CustomSvgComponent
+											classNames={'w-[25px] h-[25px] bg-white/75'}
+											url={'/party-horn.svg'}
+										/>
+									</div>
+									<div
+										className={
+											'absolute -left-[25px] -top-[15px] flex items-center justify-center'
+										}
+									>
+										<CustomSvgComponent
+											classNames={
+												'w-[25px] h-[25px] bg-white/75 transform scale-x-[-1]'
+											}
+											url={'/party-horn.svg'}
+										/>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<Image
+						src={'/background.jpg'}
+						alt={'Wedding'}
+						fill={true}
+						className={'-z-10 object-cover grayscale'}
+					/>
+				</div>
+			</div>
+			<div className={'mx-auto max-w-7xl px-2 py-16 md:px-4 xl:px-8'}>
+				second bloc
+			</div>
+		</>
+	)
+}
