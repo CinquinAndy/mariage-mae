3b36075dcd178f0afb03caf793f651027710f9bd
Cinquin Andy <andy.cinquin@infotel.com>
2023-11-26 17:08:05 +0100

    🔧 Refactor visitedPagesStore.js to save state in localStorage
    This commit refactors the `visitedPagesStore.js` file to include functionality for saving and loading state from localStorage. It introduces a new function `saveStateToLocalStorage` that serializes the state and stores it in localStorage. The initial state is now loaded from localStorage or set as an empty object if no previous state exists. The `addVisitedPage` and `removeVisitedPageIndicator` functions have been updated to also save the updated state to localStorage.

--- src/stores/visitedPagesStore.js
+++ src/stores/visitedPagesStore.js
@@ -1,16 +1,34 @@
 // stores/visitedPagesStore.js
 import create from 'zustand'
 
-const useVisitedPagesStore = create((set, get) => ({
-	visitedPages: {},
-	addVisitedPage: page =>
-		set(state => ({
-			visitedPages: { ...state.visitedPages, [page]: false },
-		})),
-	removeVisitedPageIndicator: page =>
-		set(state => ({
-			visitedPages: { ...state.visitedPages, [page]: true },
-		})),
-}))
+const useVisitedPagesStore = create((set, get) => {
+	// Function to save state to localStorage
+	const saveStateToLocalStorage = newState => {
+		try {
+			const serializedState = JSON.stringify(newState)
+			localStorage.setItem('visitedPages', serializedState)
+		} catch (e) {
+			// Handle errors or ignore
+			console.error('Could not save state to localStorage:', e)
+		}
+	}
+
+	// Load initial state from localStorage or use an empty object
+	const initialState = JSON.parse(localStorage.getItem('visitedPages')) || {}
+
+	return {
+		visitedPages: initialState,
+		addVisitedPage: page => {
+			const newState = { ...get().visitedPages, [page]: false }
+			saveStateToLocalStorage(newState)
+			set({ visitedPages: newState })
+		},
+		removeVisitedPageIndicator: page => {
+			const newState = { ...get().visitedPages, [page]: true }
+			saveStateToLocalStorage(newState)
+			set({ visitedPages: newState })
+		},
+	}
+})
 
 export default useVisitedPagesStore
