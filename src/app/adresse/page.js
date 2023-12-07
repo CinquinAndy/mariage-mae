92d8554a9f8b4c707220adbf47ad10d017274918
Cinquin Andy <andy.cinquin@infotel.com>
2023-12-07 19:13:07 +0100

    📦 Add new images and components for addresses page
    This commit adds new images (adresses.png, church.png, church_bg.png, reception_bg.png, townhall_bg.png) to the public directory. It also imports and uses the TitleOrnementedComponent, SectionComponent, and CustomSvgComponent in the adresse/page.js file. The code has been updated to include these new components and display the imported images on the addresses page.

--- src/app/adresse/page.js
+++ src/app/adresse/page.js
@@ -1,4 +1,7 @@
 import Image from 'next/image'
+import TitleOrnementedComponent from '@/components/Landing/TitleOrnemented.component'
+import { SectionComponent } from '@/components/Landing/Section.component'
+import { CustomSvgComponent } from '@/components/CustomSvg.component'
 
 export default function Page() {
 	return (
@@ -33,10 +36,109 @@ export default function Page() {
 					className={
 						'relative h-[85%] w-[90%] rounded border border-gray-100 shadow-2xl md:h-[70%] md:w-[70%] xl:h-[65%]'
 					}
-				></div>
+				>
+					<div className={'absolute h-full w-full p-3'}>
+						<div
+							className={
+								'relative flex h-full w-full items-center justify-center rounded bg-black/10'
+							}
+						>
+							<h1
+								className={
+									'text-center text-5xl text-white shadow-black text-shadow-lg lg:text-7xl xl:text-9xl'
+								}
+							>
+								Adresses
+							</h1>
+						</div>
+					</div>
+					<Image
+						src={'/adresses.png'}
+						alt={'adresses'}
+						fill={true}
+						className={'-z-10 object-cover opacity-90 grayscale'}
+					/>
+				</div>
 			</div>
-			<div className={'mx-auto max-w-7xl px-2 py-16 md:px-4 xl:px-8'}>
-				second bloc
+			<div
+				className={
+					'mx-auto flex max-w-7xl flex-col gap-32 px-2 py-16 md:px-4 xl:px-8'
+				}
+			>
+				<TitleOrnementedComponent
+					title={'Les adresses importantes'}
+					ornement={true}
+				/>
+
+				<SectionComponent
+					imageSrc={'/florida.jpg'}
+					imageAlt={'Floride'}
+					title={'Floride'}
+					isReverse={false}
+					text={
+						<p className={''}>
+							Notre prochain chapitre commence sous les palmiers et le soleil
+							éclatant de la Floride !
+							<br />
+							<br />
+							Cette terre de merveilles, connue pour ses plages de sable fin,
+							ses eaux cristallines et son ambiance ensoleillée, sera le théâtre
+							de notre aventure romantique.
+							<br />
+							<br />
+							Nous sommes impatients de vivre ces moments magiques et de les
+							partager avec vous à notre retour. Préparez-vous à entendre des
+							histoires passionnantes et à voir des photos éblouissantes de
+							notre escapade ensoleillée en Floride !
+						</p>
+					}
+				/>
+
+				<SectionComponent
+					imageSrc={'/disney.jpg'}
+					imageAlt={'disney'}
+					title={'Disney World'}
+					isReverse={true}
+					text={
+						<p className={''}>
+							Prochaine escale de notre aventure en Floride :
+							<br />
+							<br />
+							le monde enchanté de Disney World ! <br />
+							Là où la magie rencontre la réalité, nous nous préparons à plonger
+							dans un univers de contes de fées, de parades éblouissantes et de
+							moments féeriques.
+							<br />
+							<br />
+							Nous sommes impatients de partager avec vous la joie et
+							l&apos;émerveillement de cette étape fantastique de notre voyage.
+						</p>
+					}
+				/>
+
+				<SectionComponent
+					imageSrc={'/universal.jpg'}
+					imageAlt={'universal'}
+					title={'Universal Studios'}
+					isReverse={false}
+					text={
+						<p className={''}>
+							Suite à notre escapade enchantée à Disney World, nous poursuivons
+							notre aventure en Floride en nous dirigeant vers
+							l&apos;époustouflant univers d&apos;Universal Studios Orlando.
+							<br />
+							<br />
+							Là, nous plongerons ensemble dans un monde où les films prennent
+							vie, où chaque tournant révèle une nouvelle aventure palpitante.
+							<br />
+							<br />
+							Nous sommes impatients de déambuler dans les allées thématiques,
+							de nous émerveiller devant les spectacles éblouissants et de
+							ressentir l&apos;excitation des montagnes russes à couper le
+							souffle.
+						</p>
+					}
+				/>
 			</div>
 		</>
 	)
