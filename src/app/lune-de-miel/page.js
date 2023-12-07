efc4aa72a4390bc927fdc023eec39a1f618e1ad4
Cinquin Andy <andy.cinquin@infotel.com>
2023-12-07 18:03:03 +0100

    ✨ Add CustomSvgComponent to Page component
    This commit adds the CustomSvgComponent to the Page component, allowing for the display of custom SVG images. The added component is used to display inverted commas in the text section of the page.

--- src/app/lune-de-miel/page.js
+++ src/app/lune-de-miel/page.js
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import TitleOrnementedComponent from '@/components/Landing/TitleOrnemented.component'
 import { SectionComponent } from '@/components/Landing/Section.component'
+import { CustomSvgComponent } from '@/components/CustomSvg.component'
 
 export default function Page() {
 	return (
@@ -59,29 +60,71 @@ export default function Page() {
 					/>
 				</div>
 			</div>
-			<div className={'mx-auto max-w-7xl px-2 py-16 md:px-4 xl:px-8'}>
-				<TitleOrnementedComponent
-					title={'Voyage à Orlando !'}
-					ornement={true}
-				/>
+			<div
+				className={
+					'mx-auto flex max-w-7xl flex-col gap-16 px-2 py-16 md:px-4 xl:px-8'
+				}
+			>
+				<TitleOrnementedComponent title={'Voyage à Orlando'} ornement={true} />
+				<div
+					className={
+						'relative flex w-full items-center justify-center px-8 md:px-16 xl:px-32'
+					}
+				>
+					<CustomSvgComponent
+						classNames={
+							'absolute -top-4 left-4 !w-[25px] !h-[25px] md:!w-[35px] md:!h-[35px] xl:!w-[50px] xl:!h-[50px] bg-mae-950'
+						}
+						url={'inverted-commas.svg'}
+					/>
+					<p className={'text-center'}>
+						{`
+							Alors que nous nous apprêtons à entrelacer nos vies en un lien éternel de mari et femme, notre cœur bat à l'unisson pour une lune de miel de rêve sous les cieux ensoleillés de Floride, aux États-Unis.
+						`}
+						<br />
+						<br />
+						{`
+							Votre soutien, tel un fil d'or tissé dans le tapis de notre avenir, serait un présent inestimable, aidant à réaliser ce doux rêve qui brillera dans nos souvenirs comme une étoile intemporelle.
+						`}
+						<br />
+						<br />
+						{`	
+							Chaque geste, grand ou petit, est une caresse de l'amour, nous guidant vers cet instant magique où nos âmes danseront au rythme des vagues floridiennes.
+						`}
+						<br />
+						<br />
+						{`	
+							Nous vous remercions du plus profond de notre être, pour tisser avec nous les fils de cette aventure inoubliable qui marquera le début de notre vie à deux.
+						`}
+					</p>
+					<CustomSvgComponent
+						classNames={
+							'absolute right-4 -bottom-4 !w-[25px] !h-[25px] md:!w-[35px] md:!h-[35px] xl:!w-[50px] xl:!h-[50px] bg-mae-950 transform scale-x-[-1]'
+						}
+						url={'inverted-commas.svg'}
+					/>
+				</div>
+
 				<SectionComponent
-					imageSrc={'/image00014.jpeg'}
-					imageAlt={'Mariage de Mae et Romain'}
-					title={'On se dit oui !'}
+					imageSrc={'/florida.jpg'}
+					imageAlt={'Floride'}
+					title={'Floride'}
 					isReverse={false}
 					text={
 						<p className={''}>
-							{`Le 10 août 2024, nous nous dirons oui pour la vie,`}
+							Notre prochain chapitre commence sous les palmiers et le soleil
+							éclatant de la Floride !
 							<br />
-							{`Et nous vous invitons à célébrer avec nous,`}
 							<br />
-							{`Ce jour qui sera le plus beau de notre vie,`}
+							Cette terre de merveilles, connue pour ses plages de sable fin,
+							ses eaux cristallines et son ambiance ensoleillée, sera le théâtre
+							de notre aventure romantique.
 							<br />
-							{`En espérant que vous serez des nôtres,`}
 							<br />
-							{`Pour partager avec nous, ce moment de bonheur,`}
-							<br />
-							{`Et nous accompagner dans notre vie de tous les jours.`}
+							Nous sommes impatients de vivre ces moments magiques et de les
+							partager avec vous à notre retour. Préparez-vous à entendre des
+							histoires passionnantes et à voir des photos éblouissantes de
+							notre escapade ensoleillée en Floride !
 						</p>
 					}
 				/>
