db99f72ecbd84107fdb03c98856b05340b8d390e
CinquinAndy <cinquin.andy@gmail.com>
2024-03-06 16:15:14 +0100

    umami

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,7 @@ import WrapNextUiProvider from '@/providers/WrapNextUiProvider'
 import { Kanit, Updock } from 'next/font/google'
 import { NavComponent } from '@/components/Nav.component'
 import { SignatureComponent } from '@/components/SignatureFooter.component'
+import Script from 'next/script'
 
 const kanit = Kanit({
 	weight: ['100', '200', '300', '400', '700', '900'],
@@ -36,6 +37,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html className={`${kanit.variable} ${updock.variable}  text-mae-950`}>
+			<Script
+				async
+				src="https://umami.wadefade.fr/script.js"
+				strategy={'afterInteractive'}
+				data-website-id="1bf392de-37c5-4bbc-b07e-0b18d94906b2"
+			/>
 			<body className={'relative'}>
 				<NavComponent />
 				<WrapNextUiProvider>{children}</WrapNextUiProvider>
